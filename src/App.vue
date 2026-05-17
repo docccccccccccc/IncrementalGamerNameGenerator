d5f0358ca71d0d6a9ec2fab995fd669509cd1c05
doctypehtml <doctypehtml163@163.com>
2026-05-17 16:25:40 +0800

    Add basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import { ElConfigProvider } from 'element-plus'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import PageLayout from './components/PageLayout.vue'
 </script>
 
 <template>
-  <el-config-provider :locale="zhCn"> </el-config-provider>
+  <el-config-provider :locale="zhCn">
+    <PageLayout>
+      <template #header>312</template>
+      12312312
+    </PageLayout>
+  </el-config-provider>
 </template>
 
 <style scoped></style>
---
+++ src/components/PageLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-container>
+    <el-header>
+      <h1>
+        <slot name="header"></slot>
+      </h1>
+    </el-header>
+    <el-main>
+      <slot></slot>
+    </el-main>
+    <el-footer>
+      <p>Made by <el-link href="https://doctypehtml.top">doctypehtml</el-link></p>
+      <p>
+        GitHub Profile:
+        <el-link href="https://github.com/docccccccccccc">docccccccccccc (doctypehtml)</el-link>
+      </p>
+      <p>
+        GitHub repository for this project:
+        <el-link href="https://github.com/docccccccccccc/IncrementalGamerNameGenerator">
+          docccccccccccc/IncremetalGamerNameGenerator
+        </el-link>
+      </p>
+    </el-footer>
+  </el-container>
+</template>
+
+<style lang="scss" scoped>
+.el-container {
+  text-align: center;
+}
+</style>
