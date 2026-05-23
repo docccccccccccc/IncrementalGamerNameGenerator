a8281966ef710b3b3b49f56a3e8c0a10f8c81595
doctypehtml <doctypehtml163@163.com>
2026-05-24 02:03:13 +0800

    Add validator for the form

--- src/views/E308Styled.vue
+++ src/views/E308Styled.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import PageLayout from '@/components/PageLayout.vue'
-import { type FormInstance } from 'element-plus'
+import { type FormRules, type FormInstance } from 'element-plus'
 import { reactive, ref, computed } from 'vue'
 import { useRouter } from 'vue-router'
 
@@ -14,7 +14,7 @@ interface nameOptionsForm {
   }
   afterE: {
     limited: boolean
-    customizedNum: number
+    customizedNum: string // For leading 0 =)
   }
 }
 
@@ -26,11 +26,61 @@ const nameOptionsFormValue = reactive<nameOptionsForm>({
   },
   afterE: {
     limited: true,
-    customizedNum: 308,
+    customizedNum: '308',
   },
 })
 
 const nameOptionsFormRef = ref<FormInstance>()
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const checkCustomizedText = (rule: any, value: string, callback: any) => {
+  // If value is empty
+  if (!value) {
+    callback(new Error('此处不能为空'))
+  }
+
+  function isEnglishLetter(char: string) {
+    // I can't memorize ASCII well so far
+    const charCode = char.charCodeAt(0)
+    const isUppercase = charCode >= 65 && charCode <= 90
+    const isLowercase = charCode >= 97 && charCode <= 122
+    return isUppercase || isLowercase
+  }
+
+  // English letter
+  if (!value.split('').every((char: string) => isEnglishLetter(char))) {
+    callback(new Error('输入的内容不是英文字母'))
+  }
+
+  if (!(value.length === 3)) {
+    callback(new Error('输入的内容必须为 3 个字符'))
+  }
+}
+
+const checkCustomizedNum = (rule: any, value: string, callback: any) => {
+  if (!value) {
+    callback(new Error('此处不能为空'))
+  }
+
+  function isNumberChar(char: string) {
+    const charCode = char.charCodeAt(0)
+    return charCode >= 48 && charCode <= 57
+  }
+
+  if (!value.split('').every((char: string) => isNumberChar(char))) {
+    callback(new Error('输入的内容不是数字（或者你输入了其他的数字字符）'))
+  }
+
+  if (!(value.length === 3)) {
+    callback(new Error('输入的内容必须为 3 个字符'))
+  }
+}
+
+/* eslint-enable @typescript-eslint/no-explicit-any */
+const nameOptionsFormRules = reactive<FormRules<nameOptionsForm>>({
+  'beforeE.customizedText': [{ validator: checkCustomizedText, trigger: 'blur' }],
+  'afterE.customizedNum': [{ validator: checkCustomizedNum, trigger: 'blur' }],
+})
 
 const nameStylePreview = computed(() => {
   // Return 'Any' if both are unlimited
@@ -64,7 +114,11 @@ const nameStylePreview = computed(() => {
       <h3>样式预览</h3>
       <p class="name-style-preview">{{ nameStylePreview }}</p>
       <el-divider />
-      <el-form :model="nameOptionsFormValue" :ref="nameOptionsFormRef">
+      <el-form
+        :model="nameOptionsFormValue"
+        :ref="nameOptionsFormRef"
+        :rules="nameOptionsFormRules"
+      >
         <!-- Limit first -->
         <el-form-item label="取名限制">
           <el-form-item label="限制 e 前部分" prop="beforeE.limited">
@@ -110,7 +164,7 @@ const nameStylePreview = computed(() => {
         <!-- Then AfterE -->
         <!-- Unlimited only -->
         <el-form-item
-          label="e 后部分（仅允许 000 - 999）内的数字"
+          label="e 后部分（仅允许 000 - 999）内的 3 位数字"
           prop="afterE.customizedNum"
           v-if="!nameOptionsFormValue.afterE.limited"
         >
