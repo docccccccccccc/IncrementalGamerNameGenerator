7ccb7bc229c7d242042abc72e297e26b89aed4cb
doctypehtml <doctypehtml163@163.com>
2026-05-24 01:35:51 +0800

    Add v-model for the form, and remake the form

--- src/views/E308Styled.vue
+++ src/views/E308Styled.vue
@@ -1,8 +1,56 @@
 <script setup lang="ts">
 import PageLayout from '@/components/PageLayout.vue'
+import { type FormInstance } from 'element-plus'
+import { reactive, ref, computed } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
+
+interface nameOptionsForm {
+  beforeE: {
+    limited: boolean
+    customizedText: string
+    uppercase: string[]
+  }
+  afterE: {
+    limited: boolean
+    customizedNum: number
+  }
+}
+
+const nameOptionsFormValue = reactive<nameOptionsForm>({
+  beforeE: {
+    limited: true,
+    customizedText: 'AbC',
+    uppercase: ['A', 'C'],
+  },
+  afterE: {
+    limited: true,
+    customizedNum: 308,
+  },
+})
+
+const nameOptionsFormRef = ref<FormInstance>()
+
+const nameStylePreview = computed(() => {
+  // Return 'Any' if both are unlimited
+  if (!(nameOptionsFormValue.beforeE.limited || nameOptionsFormValue.afterE.limited)) {
+    return '[任意]'
+  }
+
+  const uppercase = nameOptionsFormValue.beforeE.uppercase
+
+  // BeforeE part in return value
+  const beforeE = nameOptionsFormValue.beforeE.limited
+    ? (uppercase.includes('A') ? 'A' : 'a') +
+      (uppercase.includes('B') ? 'B' : 'b') +
+      (uppercase.includes('C') ? 'C' : 'c')
+    : '[任意三个字母]'
+  // AfterE part in return value
+  const afterE = nameOptionsFormValue.afterE.limited ? '308' : '[任意三位数字]'
+
+  return beforeE + 'e' + afterE
+})
 </script>
 
 <template>
@@ -14,34 +62,59 @@ const router = useRouter()
     <el-card>
       <h2>名字选项</h2>
       <h3>样式预览</h3>
+      <p class="name-style-preview">{{ nameStylePreview }}</p>
       <el-divider />
-      <p class="name-style-preview"></p>
-      <el-form>
-        <el-form-item label="取名模式">
-          <el-radio-group>
-            <el-radio-button label="我自己取" value="free" />
-            <el-radio-button label="帮我生成" value="generative" />
-          </el-radio-group>
+      <el-form :model="nameOptionsFormValue" :ref="nameOptionsFormRef">
+        <!-- Limit first -->
+        <el-form-item label="取名限制">
+          <el-form-item label="限制 e 前部分" prop="beforeE.limited">
+            <el-switch v-model="nameOptionsFormValue.beforeE.limited" />
+          </el-form-item>
+          <el-form-item label="限制 e 后部分" prop="afterE.limited">
+            <el-switch v-model="nameOptionsFormValue.afterE.limited" />
+          </el-form-item>
         </el-form-item>
-        <template>
-          <el-form-item label="前半部分（仅允许字母）">
-            <el-input />
-          </el-form-item>
-          <el-form-item label="后半部分（仅允许 000 - 999）内的数字">
-            <el-input />
-          </el-form-item>
-        </template>
-        <template>
-          <el-form-item label="前半部分字母大小写选项">
-            <el-checkbox-group>
-              <el-checkbox-button>X</el-checkbox-button>
-              <el-checkbox-button>x</el-checkbox-button>
-              <el-checkbox-button>X</el-checkbox-button>
-            </el-checkbox-group>
-          </el-form-item>
-        </template>
-        <el-form-item label="限制后半部分数字为 308">
-          <el-switch />
+        <!-- Then, handle 2 parts -->
+        <!-- BeforeE first -->
+
+        <!-- Unlimited -->
+        <el-form-item
+          label="e 前部分（仅允许字母）"
+          prop="beforeE.customizedText"
+          v-if="!nameOptionsFormValue.beforeE.limited"
+        >
+          <el-input v-model="nameOptionsFormValue.beforeE.customizedText" />
+        </el-form-item>
+
+        <!-- Limited -->
+        <el-form-item
+          label="e 前部分字母大小写选项"
+          prop="beforeE.uppercase"
+          v-if="nameOptionsFormValue.beforeE.limited"
+        >
+          <el-checkbox-group v-model="nameOptionsFormValue.beforeE.uppercase">
+            <el-checkbox-button
+              :value="checkboxBtn"
+              v-for="checkboxBtn in 'ABC'"
+              :key="checkboxBtn"
+            >
+              {{
+                nameOptionsFormValue.beforeE.uppercase.includes(checkboxBtn)
+                  ? checkboxBtn
+                  : checkboxBtn.toLowerCase()
+              }}
+            </el-checkbox-button>
+          </el-checkbox-group>
+        </el-form-item>
+
+        <!-- Then AfterE -->
+        <!-- Unlimited only -->
+        <el-form-item
+          label="e 后部分（仅允许 000 - 999）内的数字"
+          prop="afterE.customizedNum"
+          v-if="!nameOptionsFormValue.afterE.limited"
+        >
+          <el-input v-model="nameOptionsFormValue.afterE.customizedNum" />
         </el-form-item>
       </el-form>
     </el-card>
