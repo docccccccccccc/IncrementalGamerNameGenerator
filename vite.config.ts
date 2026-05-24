d45dfb6bdfb3bdcd47f8ad43b9f84587b8cfc0c6
doctypehtml <doctypehtml163@163.com>
2026-05-24 21:05:00 +0800

    Revert vite.config.json, and package.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,49 +3,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import obfuscatorPlugin from 'vite-plugin-obfuscator-ts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    process.env.NODE_ENV === 'production' &&
-      obfuscatorPlugin({
-        // include: [/\.js$/, /\.ts$/, /\.vue$/],
-        exclude: ['node_modules/**'],
-        obfuscatorOptions: {
-          // Basic compression
-          compact: true, // Remove space
-          simplify: true,
-
-          // Core protect
-          stringArray: true,
-          stringArrayEncoding: ['base64'], // Encode string
-          stringArrayThreshold: 0.8, // 80%
-
-          numbersToExpressions: true,
-
-          controlFlowFlattening: true,
-          controlFlowFlatteningThreshold: 0.7,
-          deadCodeInjection: true, // Dead code
-          deadCodeInjectionThreshold: 0.4,
-
-          identifierNamesGenerator: 'hexadecimal',
-
-          disableConsoleOutput: true,
-          debugProtection: false,
-          selfDefending: false,
-        },
-      }),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   build: {
-    sourcemap: false,
     minify: 'terser',
     terserOptions: {
       compress: {
